2a93ae0f64f292f9796c2adb86c8d4e6c4909ac8
jagilley <37783831+jagilley@users.noreply.github.com>
2020-12-14 00:19:34 -0600

    d4

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 import requests
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    #r = requests.get('http://httpbin.org/status/418')
+    #template = loader.get_template('templates/base.html')
+    return render(request, "templates/base.html")
 
 
 def db(request):
