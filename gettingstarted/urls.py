f4fca46562d6ead81204caba29be368e544c129e
jagilley <37783831+jagilley@users.noreply.github.com>
2020-12-14 17:23:27 -0600

    d88

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -17,5 +17,6 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
+    path("grid/", hello.views.grid, name="grid"),
     path("admin/", admin.site.urls),
 ]
