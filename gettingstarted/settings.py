191f75d503acd7d2c8d1e8d3a840a82328da6bd3
jagilley <37783831+jagilley@users.noreply.github.com>
2020-12-18 19:38:03 -0600

    gi2

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be used, if set, instead)."
+# added some text
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
